b79927250645f1da98e0c37f550bfa736e133552
aabbcc-nwjian <2090364287@qq.com>
2025-01-23 21:07:58 +0800

    Add files via upload

---
+++ app/(tabs)/shouye/index.jsx
@@ -0,0 +1,20 @@
+import { useEffect ,useState} from 'react';
+import { Button, Image, ScrollView, StyleSheet, TextInput } from 'react-native';
+import { View , Text ,Alert} from 'react-native';
+import React from 'react';
+
+export default function Follow() {
+  return (
+    <>
+    <View style={styles.container}>
+      <Text>练习</Text>
+    </View>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  container:{
+  marginTop:'70%',
+  marginLeft:'46%'
+  }
+})
